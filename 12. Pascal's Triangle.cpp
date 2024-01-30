a40e4ee09b07a14cf88a3eb75ada7fc6c09bbb85
Santhosh Reddy <112513931+Santhoshreddy9278@users.noreply.github.com>
2024-01-30 13:14:44 +0530

    Update 12. Pascal's Triangle.cpp

--- 12. Pascal's Triangle.cpp
+++ 12. Pascal's Triangle.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Function to calculate binomial coefficient
 int binomialCoeff(int n, int k) {
   if (k == 0 || k == n) {
     return 1;
@@ -9,15 +8,12 @@ int binomialCoeff(int n, int k) {
   }
 }
 
-// Function to print a row of Pascal's Triangle
 void printPascalRow(int n, int row) {
   for (int k = 0; k <= row; k++) {
     printf("%d ", binomialCoeff(n, k));
   }
   printf("\n");
 }
-
-// Function to print Pascal's Triangle for n rows
 void printPascalTriangle(int n) {
   for (int row = 0; row < n; row++) {
     printPascalRow(n, row);
@@ -35,4 +31,3 @@ int main() {
 
   return 0;
 }
-
