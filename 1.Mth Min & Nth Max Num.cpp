6c44056bad6e0c4c3b645086846d7c09c21f0869
Santhosh Reddy <112513931+Santhoshreddy9278@users.noreply.github.com>
2024-01-31 08:32:53 +0530

    Day 1 - Set 1

--- 1.Mth Min & Nth Max Num.cpp
+++ 1.Mth Min & Nth Max Num.cpp
@@ -1,26 +1,64 @@
 #include <stdio.h>
 
+void findMaxMin(int arr[], int size, int m, int n, int *max, int *min) {
+    
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+   
+    *max = arr[size - m];
+    *min = arr[n - 1];
+}
+
 int main() {
-  int arr[] = {16, 16, 16, 16, 16};
-  int M = 0;
-  int N = 1;
+    int size, m, n;
+    
+    
+    do {
+        printf("Enter the size of the array (should be greater than 1): ");
+        scanf("%d", &size);
+    } while (size <= 1);
 
-  int max = arr[0];
-  for (int i = 1; i < N; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
+    
+    int arr[size];
+    printf("Enter the array elements:\n");
+    for (int i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
     }
-  }
 
-  int min = arr[0];
-  for (int i = 1; i < N; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-  }
+   
+    do {
+        printf("Enter the value of m (1 <= m <= %d): ", size);
+        scanf("%d", &m);
+    } while (m < 1 || m > size);
 
-  printf("Sum = %d\n", max + min);
-  printf("Difference = %d\n", max - min);
+    do {
+        printf("Enter the value of n (1 <= n <= %d): ", size);
+        scanf("%d", &n);
+    } while (n < 1 || n > size);
 
-  return 0;
+    int max, min;
+
+    
+    findMaxMin(arr, size, m, n, &max, &min);
+
+   
+    int sum = max + min;
+    int diff = max - min;
+
+    printf("The %dth maximum number is: %d\n", m, max);
+    printf("The %dth minimum number is: %d\n", n, min);
+    printf("Sum of the %dth maximum and %dth minimum numbers: %d\n", m, n, sum);
+    printf("Difference between the %dth maximum and %dth minimum numbers: %d\n", m, n, diff);
+
+    return 0;
 }
