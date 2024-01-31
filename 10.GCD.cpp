3500a9f20feb2832f91f4bb2059df63f0214b8a3
Santhosh Reddy <112513931+Santhoshreddy9278@users.noreply.github.com>
2024-01-31 12:47:02 +0530

    Day 1 - Set 3

--- 10.GCD.cpp
+++ 10.GCD.cpp
@@ -1,24 +1,32 @@
 #include <stdio.h>
 
+
 int gcd(int a, int b) {
-  if (b == 0) {
-    return a;
-  } else {
-    return gcd(b, a % b); 
-  }
+    if (b == 0) {
+        return a;
+    } else {
+        return gcd(b, a % b);
+    }
 }
 
 int main() {
-  int num1, num2;
+    int num1, num2;
 
-  printf("Enter first number: ");
-  scanf("%d", &num1);
-  printf("Enter second number: ");
-  scanf("%d", &num2);
+   
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
 
-  int result = gcd(num1, num2);
-  printf("GCD of %d and %d is %d\n", num1, num2, result);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
 
-  return 0;
+   
+    if (num1 < 0 || num2 < 0) {
+        printf("GCD is not defined for negative numbers.\n");
+    } else {
+       
+        int result = gcd(num1, num2);
+        printf("GCD of %d and %d is: %d\n", num1, num2, result);
+    }
+
+    return 0;
 }
-
