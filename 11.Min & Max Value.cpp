ef04a32b33658a71666f9eff20aeeb17d4bd3fa9
Santhosh Reddy <112513931+Santhoshreddy9278@users.noreply.github.com>
2024-01-31 12:48:10 +0530

    Day 1 - Set 3

--- 11.Min & Max Value.cpp
+++ 11.Min & Max Value.cpp
@@ -1,41 +1,55 @@
-#include <stdio.h>
-
-int minMax(int arr[], int low, int high, int* comparisons) {
-  if (low == high) {
-    *comparisons += 2;
-    return arr[low];
-  } else if (low + 1 == high) {
-    *comparisons += 3; 
-    if (arr[low] < arr[high]) {
-      return arr[low];
-    } else {
-      return arr[high];
-    }
-  } else {
-    int mid = (low + high) / 2;
-    int minLeft = minMax(arr, low, mid, comparisons);
-    int minRight = minMax(arr, mid + 1, high, comparisons);
-    *comparisons += 1;
-    return minLeft < minRight ? minLeft : minRight;
+#include<stdio.h>
+#include<stdio.h>
+int max, min;
+int a[100];
+void maxmin(int i, int j)
+{
+ int max1, min1, mid;
+ if(i==j)
+ {
+  max = min = a[i];
+ }
+ else
+ {
+  if(i == j-1)
+  {
+   if(a[i] <a[j])
+   {
+    max = a[j];
+    min = a[i];
+   }
+   else
+   {
+    max = a[i];
+    min = a[j];
+   }
   }
+  else
+  {
+   mid = (i+j)/2;
+   maxmin(i, mid);
+   max1 = max; min1 = min;
+   maxmin(mid+1, j);
+   if(max <max1)
+    max = max1;
+   if(min > min1)
+    min = min1;
+  }
+ }
 }
+int main ()
+{
+ int i, num;
+ printf ("\nEnter the total number of numbers : ");
+ scanf ("%d",&num);
+ printf ("Enter the numbers : \n");
+ for (i=1;i<=num;i++)
+  scanf ("%d",&a[i]);
 
-int main() {
-  int n;
-  printf("Enter the number of elements: ");
-  scanf("%d", &n);
-
-  int arr[n];
-  printf("Enter the elements: ");
-  for (int i = 0; i < n; i++) {
-    scanf("%d", &arr[i]);
-  }
-
-  int comparisons = 0;
-  int minMaxVal = minMax(arr, 0, n - 1, &comparisons);
-
-  printf("Minimum and maximum value: %d\n", minMaxVal);
-  printf("Number of comparisons: %d\n", comparisons);
-
-  return 0;
+ max = a[0];
+ min = a[0];
+ maxmin(1, num);
+ printf ("Minimum element in an array : %d\n", min);
+ printf ("Maximum element in an array : %d\n", max);
+ return 0;
 }
