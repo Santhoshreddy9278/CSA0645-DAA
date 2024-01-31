e35d0b5c0a961cb5f7904aff430b7953d16018c4
Santhosh Reddy <112513931+Santhoshreddy9278@users.noreply.github.com>
2024-01-31 08:56:40 +0530

    Day 1 = set 2

--- 7.Strassen's Matrix.cpp
+++ 7.Strassen's Matrix.cpp
@@ -1,46 +1,52 @@
-#include <stdio.h>
+#include<stdio.h>
+int main()
+{
+  int a[2][2],b[2][2],c[2][2],i,j;
+  int m1,m2,m3,m4,m5,m6,m7;
 
-void strassen(int A[2][2], int B[2][2], int C[2][2]) {
-  int p1, p2, p3, p4, p5, p6, p7;
+  printf("Enter the 4 elements of first matrix: ");
+  for(i=0;i<2;i++)
+      for(j=0;j<2;j++)
+           scanf("%d",&a[i][j]);
 
-  p1 = A[0][0] * B[0][0];
-  p2 = A[1][0] * B[0][1];
-  p3 = A[0][0] * B[1][0];
-  p4 = A[1][0] * B[1][1];
-  p5 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
-  p6 = (A[0][1] - A[1][1]) * (B[0][0] + B[0][1]);
-  p7 = (A[0][0] - A[0][1]) * (B[1][0] - B[1][1]);
+  printf("Enter the 4 elements of second matrix: ");
+  for(i=0;i<2;i++)
+      for(j=0;j<2;j++)
+           scanf("%d",&b[i][j]);
 
-  C[0][0] = p1 + p4 - p5 + p7;
-  C[0][1] = p2 + p4;
-  C[1][0] = p3 + p6;
-  C[1][1] = p1 + p3 - p2 + p5;
-}
-
-void printMatrix(int A[2][2]) {
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 2; j++) {
-      printf("%d ", A[i][j]);
-    }
-    printf("\n");
+  printf("\nThe first matrix is\n");
+  for(i=0;i<2;i++){
+      printf("\n");
+      for(j=0;j<2;j++)
+           printf("%d\t",a[i][j]);
   }
-}
 
-int main() {
-  int A[2][2] = {{1, 2}, {3, 4}};
-  int B[2][2] = {{5, 6}, {7, 8}};
-  int C[2][2];
+  printf("\nThe second matrix is\n");
+  for(i=0;i<2;i++){
+      printf("\n");
+      for(j=0;j<2;j++)
+           printf("%d\t",b[i][j]);
+  }
 
-  strassen(A, B, C);
+  m1= (a[0][0] + a[1][1])*(b[0][0]+b[1][1]);
+  m2= (a[1][0]+a[1][1])*b[0][0];
+  m3= a[0][0]*(b[0][1]-b[1][1]);
+  m4= a[1][1]*(b[1][0]-b[0][0]);
+  m5= (a[0][0]+a[0][1])*b[1][1];
+  m6= (a[1][0]-a[0][0])*(b[0][0]+b[0][1]);
+  m7= (a[0][1]-a[1][1])*(b[1][0]+b[1][1]);
 
-  printf("Matrix A:\n");
-  printMatrix(A);
+  c[0][0]=m1+m4-m5+m7;
+  c[0][1]=m3+m5;
+  c[1][0]=m2+m4;
+  c[1][1]=m1-m2+m3+m6;
 
-  printf("Matrix B:\n");
-  printMatrix(B);
+   printf("\nAfter multiplication using strassens matrix multiplication \n");
+   for(i=0;i<2;i++){
+      printf("\n");
+      for(j=0;j<2;j++)
+           printf("%d\t",c[i][j]);
+   }
 
-  printf("Matrix C (A x B):\n");
-  printMatrix(C);
-
-  return 0;
+   return 0;
 }
