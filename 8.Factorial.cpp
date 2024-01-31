ee3ac55be7ad4485ad51eadc72fb893129228da4
Santhosh Reddy <112513931+Santhoshreddy9278@users.noreply.github.com>
2024-01-31 09:06:48 +0530

    Day 1 - set 2

--- 8.Factorial.cpp
+++ 8.Factorial.cpp
@@ -1,21 +1,28 @@
 #include <stdio.h>
 
+
 int factorial(int n) {
-  if (n == 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);  
-  }
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 }
 
 int main() {
-  int number;
+    int num;
 
-  printf("Enter a number: ");
-  scanf("%d", &number);
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-  int result = factorial(number);
-  printf("%d! is %d\n", number, result);
+    
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+       
+        printf("Factorial of %d is: %d\n", num, factorial(num));
+    }
 
-  return 0;
+    return 0;
 }
